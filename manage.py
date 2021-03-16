6697bd912177c46e86255fde9bc2d0341ad2b3aa
gloria <gloria.batlle@gmail.com>
2021-03-16 14:24:25 -0400

    Mi

--- manage.py
+++ manage.py
@@ -4,11 +4,8 @@ import os
 import sys
 
 
-def main():
-    """Run administrative tasks.
-cambiando algo
-"""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,7 +15,3 @@ cambiando algo
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
