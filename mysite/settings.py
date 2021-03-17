7425977775bd650f5cec3f3ec33645a4d82db79d
gloria <gloria.batlle@gmail.com>
2021-03-17 14:15:18 -0400

    Mi

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'r=)*s_of(cw#sdnnyf-lcf*1-srim_w8@ak@iz=0$ff=)q!ywq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'os.pythonanywhere.com','http://gloriamiranda.pythonanywhere.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'os.pythonanywhere.com','gloriamiranda.pythonanywhere.com']
 
 
 # Application definition
